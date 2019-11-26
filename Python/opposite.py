db952b1db053a3fc446674d9ef4715ddbd332e02
Paul Lave <paullave@Pauls-MacBook-Pro.local>
2019-11-26 11:31:32 -0700

    Changing folder setup

--- Python/opposite.py
+++
@@ -1,9 +0,0 @@
-# Simple function returns the opposite of the number passed in
-def opposite(number):
-  return number * -1
-  
-# SAMPLE TESTS
-assert opposite(1) == -1
-assert opposite(30) == -30
-assert opposite(-10) == 10
-assert opposite(-45) == 45
